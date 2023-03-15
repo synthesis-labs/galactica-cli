419251fd8a60914c3e7ebb895a15b3c8ee0868c3
benpalay <ben@synthesis.co.za>
2023-03-15 21:13:02 +0200

    Added support for runtime detection of operating system and choosing the appropriate text editor, rather than relying on compile-time checks.
    Done in order to improve the flexibility of running the script across multiple environments.

--- src/integrations.rs
+++ src/integrations.rs
@@ -99,25 +99,32 @@ fn create_hook(filepath: &str, script: &str) -> Result<(), ClientError> {
 }
 
 fn create_pre_commit_hook() -> Result<(), ClientError> {
-    
+    let mut editor = "";
+    let os = std::env::consts::OS; //this allows the executable to run on any machine as it checks at runtime as opposed to compile time with cfg!(target_os)
+    match os {
+        "linux" => editor = "vi",
+        "macos" => editor = "TextEdit",
+        "windows" => editor = "notepad",
+        _ => editor = "vi",
+    }
+  
     let tmp_file_creator = r#"#!/bin/bash
-    if [ -n "$GIT_EDITOR" ]; then
-    exit 0
-    fi
-    TMPFILE=$(mktemp) || { echo "Failed to create temp file"; exit 1; }
-    git diff --staged | ./target/debug/galactica code 'provide 1 sentence as a summary of the changes made to this code. Then skip a line and provide a short description of why the major changes were made, using bullet points if necessary.' > "$TMPFILE"
-    "#.to_string();
-    let var_string =  r#"${EDITOR:-$(git config --get core.editor || echo 'notepad')} "$TMPFILE""#;
-    let commit_string = r#"COMMIT_MSG=$(cat "$TMPFILE")
-    rm "$TMPFILE"
-    echo "$COMMIT_MSG" | git commit -F -"#;
+if [ -n "$GIT_EDITOR" ]; then
+exit 0
+fi
+TMPFILE=$(mktemp) || { echo "Failed to create temp file"; exit 1; }
+git diff --staged | ./target/debug/galactica code 'provide 1 sentence as a summary of the changes made to this code. Then skip a line and provide a short description of why the major changes were made, using bullet points if necessary.' > "$TMPFILE"
+${EDITOR:-$(git config --get core.editor || echo '"#.to_string();
+    let commit_string = r#"')} "$TMPFILE"
+COMMIT_MSG=$(cat "$TMPFILE")
+rm "$TMPFILE"
+echo "$COMMIT_MSG" | git commit -F -"#.to_string();
     
-    let combined_str = format!("{}\n{}\n{}",tmp_file_creator, var_string, commit_string);
+    let script = format!("{}{}{}",tmp_file_creator, editor, commit_string);
 
     create_hook(
         PRE_COMMIT_HOOK_FILEPATH,
-        &combined_str
-     ,
+        &script,
     )
 }
 
